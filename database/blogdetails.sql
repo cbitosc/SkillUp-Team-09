5bd79c42f00a7719d9a22150a02d27ebe057df43
vishnupriyagoud <vishnupriyagangarapu@gmail.com>
2020-06-15 13:55:11 +0530

    Database addition

---
+++ database/blogdetails.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM studentblog.blog
+LIMIT 0, 1000
+
+-- Date: 2020-06-15 13:12
+*/
+INSERT INTO `` (`rollno`,`name`,`post_date`,`post_title`,`post_content`,`reported_user`) VALUES (1,'John Smith','2020-06-15 02:15:30','My First Blog','Hello guys!!',NULL);
+INSERT INTO `` (`rollno`,`name`,`post_date`,`post_title`,`post_content`,`reported_user`) VALUES (1,'John Smith','2020-06-16 10:15:30','Blog 2','Hello!!',NULL);
---
+++ database/userdetails.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM studentblog.user
+LIMIT 0, 1000
+
+-- Date: 2020-06-15 13:14
+*/
+INSERT INTO `` (`rollno`,`name`,`signup_date`,`last_login`,`reported_users`) VALUES (1,'John Smith','2020-06-15 01:15:30','2020-06-15 01:45:30',NULL);
+INSERT INTO `` (`rollno`,`name`,`signup_date`,`last_login`,`reported_users`) VALUES (2,'Edwin','2020-06-14 10:15:30','2020-06-15 01:45:30',NULL);
+INSERT INTO `` (`rollno`,`name`,`signup_date`,`last_login`,`reported_users`) VALUES (3,'Edward','2020-06-20 20:15:30','2020-06-20 01:45:30',NULL);
+INSERT INTO `` (`rollno`,`name`,`signup_date`,`last_login`,`reported_users`) VALUES (5,'Alwin','2020-06-10 13:15:30','2020-06-12 13:35:30',NULL);
+INSERT INTO `` (`rollno`,`name`,`signup_date`,`last_login`,`reported_users`) VALUES (18,'Andrea','2020-05-10 06:15:30','2020-05-12 10:35:30',NULL);
